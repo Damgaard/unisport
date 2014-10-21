791c8ccf349824095e0114d09896bc2073d86283
Andreas Damgaard Pedersen <andreas.damgaard.pedersen@gmail.com>
2014-10-21 21:57:03 +0100

    Create Product model.

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from models import Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'delivery', 'free_porto', 'kids', 'kids_adult',
+                    'women', 'package')
+    list_filter = ('delivery', 'free_porto', 'kids', 'kids_adult', 'women',
+                   'package')
+
+
+admin.site.register(Product, ProductAdmin)
